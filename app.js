781857ffb5c924307eabb2cab5931efb6978d2cf
jackneedham <jackpneedham@gmail.com>
2020-02-09 15:50:55 +0000

    no message

--- app.js
+++ app.js
@@ -4,18 +4,28 @@ const main = document.querySelector('main');
 var left_controls = document.getElementById("left-btn-container");
 var video_player = document.getElementById("video-player");
 var left_controls_height = left_controls.offsetHeight;
-left_controls.style.top = ((window.innerHeight -left_controls_height)/2) + "px";
-console.log(left_controls_height);
+
+
+setVideoPlayerSize();
+window.addEventListener("resize", setVideoPlayerSize);
+
+function setVideoPlayerSize() {
+	var v = document.getElementById("video-player");
+    var width = this.videoWidth,
+        height = this.videoHeight;
+
+		v.style.width = window.innerWidth+"px";
+	var aspectRatio = +(window.innerWidth/window.innerHeight);
+	if (aspectRatio > 1.33) {
+		v.style.height = (window.innerHeight-10)+"px";
+	} else {
+		v.style.width = (window.innerWidth-10)+"px";
+	}
+}
 
 document.getElementById("video-player").addEventListener("click", function(){
-	var controls = document.getElementById("controls")
-	console.log(controls.style.visibility);
-	if (controls.style.visibility == "visible") {
-		controls.style.visibility = "hidden";
-	} else {
-		controls.style.visibility = "visible";
-	}
-})
+	$("#controls").fadeToggle();
+});
 
 document.getElementById("play-btn").addEventListener("click", function() {
 	var video = document.getElementById("video-player")
@@ -26,3 +36,40 @@ document.getElementById("pause-btn").addEventListener("click", function() {
 	var video = document.getElementById("video-player")
 	video.pause();
 });
+
+document.getElementById("video-player").addEventListener("ended", function() {
+	document.getElementById("video-player").currentTime = 0;
+})
+
+function skipToTime(t) {
+	var video = document.getElementById("video-player");
+	video.currentTime = document.getElementById(t).getAttribute("time-code")
+	video.play();
+}
+
+var myTimeout = null;
+var resetTimeout = null;
+
+document.onclick = function(){
+	clearTimeout(myTimeout);
+	clearTimeout(resetTimeout);
+	myTimeout = setTimeout(function() {
+		var controls = document.getElementById("controls");
+		var video = document.getElementById("video-player");
+		if (video.currentTime > 0 && !video.paused) {
+
+			console.log(controls.style.visibility);
+			$("#controls").fadeOut();
+		}
+	}, 2000);
+	resetTimeout = setTimeout(function() {
+		var video = document.getElementById("video-player");
+		if (video.paused) {
+			video.currentTime = 0;
+			video.pause();
+			$("#controls").fadeIn();
+		}
+
+	}, 30000)
+}
+
